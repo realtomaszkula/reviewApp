690b13965f584d77b8274b108930ab9dccad4955
Tomasz Kula <tomaszkula07@gmail.com>
2016-08-09 02:46:59 +0200

    Add tests for setBoard

---
+++ scripts/js/scripts/handhistories/handhistories.js
@@ -0,0 +1,44 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.one = `PokerStars Hand #156701873519:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:25:01 CET [2016/07/31 18:25:01 ET]
+Table 'Aletheia' 6-max Seat #1 is the button
+Seat 1: charlybumbum ($100 in chips) 
+Seat 2: zocker jo433 ($416.52 in chips) 
+Seat 3: Yoo4 ($347.17 in chips) 
+Seat 5: falco_lucky7 ($108.80 in chips) 
+Seat 6: reppinR1 ($116.08 in chips) 
+zocker jo433: posts small blind $0.50
+Yoo4: posts big blind $1
+*** HOLE CARDS ***
+Dealt to reppinR1 [7s 4c Ac Kh]
+falco_lucky7: folds 
+reppinR1: raises $2 to $3
+charlybumbum: folds 
+zocker jo433: calls $2.50
+Yoo4: calls $2
+*** FLOP *** [Jd Tc 2c]
+zocker jo433: checks 
+Yoo4: bets $6
+reppinR1: calls $6
+zocker jo433: folds 
+*** TURN *** [Jd Tc 2c] [Qh]
+Yoo4: checks 
+reppinR1: bets $18
+Yoo4: calls $18
+*** RIVER *** [Jd Tc 2c Qh] [Th]
+Yoo4: bets $42
+reppinR1: folds 
+Uncalled bet ($42) returned to Yoo4
+Yoo4 collected $54.50 from pot
+Yoo4: doesn't show hand 
+*** SUMMARY ***
+Total pot $57 | Rake $2.50 
+Board [Js Tc 2c 4s 5s]
+Seat 1: charlybumbum (button) folded before Flop (didn't bet)
+Seat 2: zocker jo433 (small blind) folded on the Flop
+Seat 3: Yoo4 (big blind) collected ($54.50)
+Seat 5: falco_lucky7 folded before Flop (didn't bet)
+Seat 6: reppinR1 folded on the River
+`;
+});
+//# sourceMappingURL=handhistories.js.map
--- tests/app_spec.ts
+++ tests/app_spec.ts
@@ -1,10 +1,42 @@
-
-import syntax = require('../scripts/app')
+import app = require('../scripts/app')
+import exampleHH = require('../scripts/handhistories/handhistories') 
 
 describe('Hand History', () => {
   describe('setBoard', () => {
-    it('should create board', () => {
-      
+    let hh;
+    beforeEach(() => {
+      let hhraw = `Total pot $57 | Rake $2.50 Board [Js Tc 2d 4s 5s] Seat 1:`;
+      hh = new app.HandHistory(hhraw, { setBoard: true });
+    })
+    it('should create board with flop turn and river', () => {
+      expect(hh.board.flop).toBeDefined();
+      expect(hh.board.turn).toBeDefined();
+      expect(hh.board.river).toBeDefined();
     });
+
+    describe('when creating flop', () => {
+      it('should have 3 elements', () {
+        expect(hh.board.flop.length).toEqual(3);
+      })
+      it('should create correct card value for first element', () {
+        expect(hh.board.flop[0]).toEqual('J');
+      })
+      it('should create correct card value for second element', () {
+        expect(hh.board.flop[0]).toEqual('T');
+      })
+      it('should create correct card value for third element', () {
+        expect(hh.board.flop[1]).toEqual('2');
+      })
+      it('should create correct card suit for first element', () {
+        expect(hh.board.flop[1]).toEqual('s');
+      })
+      it('should create correct card suit for second element', () {
+        expect(hh.board.flop[2]).toEqual('c');
+      })
+      it('should create correct card suit for third element', () {
+        expect(hh.board.flop[2]).toEqual('d');
+      })
+
+    })
   });
 });
