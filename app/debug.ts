04a848b07ae8c231a191a975d271dfde4cf52086
Tomasz Kula <tomaszkula07@gmail.com>
2016-08-14 05:23:09 +0200

    Add more tests for position

--- app/debug.ts
+++ app/debug.ts
@@ -1,12 +1,16 @@
 import HandHistory from "./app";
 
 let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
-Table 'Aletheia' 6-max Seat #1 is the button
+Table 'Aletheia' 6-max Seat #2 is the button
 Seat 1: Phil ($149.08 in chips) 
+Seat 2: marquim1980 ($117.77 in chips) 
+Seat 3: Yoo4 ($100 in chips) 
+Seat 4: Teiti14 ($27.49 in chips) 
+Seat 5: MiPwnYa ($107.49 in chips) 
 Seat 6: reppinR1 ($246.97 in chips) 
 Teiti14: posts small blind $0.50
 reppinR1: posts big blind $1
 *** HOLE CARDS ***
-Dealt to reppinR1 [3s 9c Ac Td]`
+  Dealt to reppinR1 [3s 9c Ac Td]`
 
 let hh = new HandHistory({ hh:hhraw, options: { setHeroName:true, setPlayers: true, setHeroPosition: true } } )
--- spec/setPlayers_spec.ts
+++ spec/setPlayers_spec.ts
@@ -9,11 +9,11 @@ describe('HandHistory', () => {
 
         setting hero position requires hero name to be found first
     */
-
-    describe('when 6 players seated', () => {
-      let hh;
-      beforeEach(() => {
-let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+    describe('when first seat is the btn', () => {
+      describe('and 6 players seated', () => {
+        let hh;
+        beforeEach(() => {
+  let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
 Table 'Aletheia' 6-max Seat #1 is the button
 Seat 1: Phil ($149.08 in chips) 
 Seat 2: marquim1980 ($117.77 in chips) 
@@ -24,23 +24,23 @@ Seat 6: reppinR1 ($246.97 in chips)
 Teiti14: posts small blind $0.50
 reppinR1: posts big blind $1
 *** HOLE CARDS ***
-Dealt to reppinR1 [3s 9c Ac Td]`
-        hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+  Dealt to reppinR1 [3s 9c Ac Td]`
+          hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+        })
+
+        it('should capture hero seat', () => {     
+          expect(hh.hero.position).toEqual('CO');
+        });
+
+        it('should capture numbers of players seated', () => {     
+          expect(hh._players.length).toEqual(6);
+        });
       })
 
-      it('should capture hero seat', () => {     
-        expect(hh.hero.position).toEqual('CO');
-      });
-
-      it('should capture numbers of players seated', () => {     
-        expect(hh._players.length).toEqual(6);
-      });
-    })
-
-    describe('when 5 players seated', () => {
-      let hh;
-      beforeEach(() => {
-let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+      describe('when 5 players seated', () => {
+        let hh;
+        beforeEach(() => {
+  let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
 Table 'Aletheia' 6-max Seat #1 is the button
 Seat 1: Phil ($149.08 in chips) 
 Seat 2: marquim1980 ($117.77 in chips) 
@@ -51,22 +51,22 @@ Teiti14: posts small blind $0.50
 reppinR1: posts big blind $1
 *** HOLE CARDS ***
 Dealt to reppinR1 [3s 9c Ac Td]`
-        hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
-      })
+          hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+        })
 
-      it('should capture hero seat', () => {     
-        expect(hh.hero.position).toEqual('MP');
+        it('should capture hero seat', () => {     
+          expect(hh.hero.position).toEqual('MP');
+        });
+
+        it('should capture numbers of players seated', () => {     
+          expect(hh._players.length).toEqual(5);
+        });
       });
 
-      it('should capture numbers of players seated', () => {     
-        expect(hh._players.length).toEqual(5);
-      });
-    });
-
-    describe('when 4 players seated', () => {
-      let hh;
-      beforeEach(() => {
-let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+      describe('when 4 players seated', () => {
+        let hh;
+        beforeEach(() => {
+  let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
 Table 'Aletheia' 6-max Seat #1 is the button
 Seat 1: Phil ($149.08 in chips) 
 Seat 2: marquim1980 ($117.77 in chips) 
@@ -76,22 +76,22 @@ Teiti14: posts small blind $0.50
 reppinR1: posts big blind $1
 *** HOLE CARDS ***
 Dealt to reppinR1 [3s 9c Ac Td]`
-        hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+          hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+        })
+
+        it('should capture hero seat', () => {     
+          expect(hh.hero.position).toEqual('UTG');
+        });
+
+        it('should capture numbers of players seated', () => {     
+          expect(hh._players.length).toEqual(4);
+        });
       })
 
-      it('should capture hero seat', () => {     
-        expect(hh.hero.position).toEqual('UTG');
-      });
-
-      it('should capture numbers of players seated', () => {     
-        expect(hh._players.length).toEqual(4);
-      });
-    })
-
-    describe('when 3 players seated', () => {
-      let hh;
-      beforeEach(() => {
-let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+      describe('when 3 players seated', () => {
+        let hh;
+        beforeEach(() => {
+  let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
 Table 'Aletheia' 6-max Seat #1 is the button
 Seat 1: Phil ($149.08 in chips) 
 Seat 2: marquim1980 ($117.77 in chips) 
@@ -100,22 +100,22 @@ Teiti14: posts small blind $0.50
 reppinR1: posts big blind $1
 *** HOLE CARDS ***
 Dealt to reppinR1 [3s 9c Ac Td]`
-        hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+          hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+        })
+
+        it('should capture hero seat', () => {     
+          expect(hh.hero.position).toEqual('BB');
+        });
+
+        it('should capture numbers of players seated', () => {     
+          expect(hh._players.length).toEqual(3);
+        });
       })
 
-      it('should capture hero seat', () => {     
-        expect(hh.hero.position).toEqual('BB');
-      });
-
-      it('should capture numbers of players seated', () => {     
-        expect(hh._players.length).toEqual(3);
-      });
-    })
-
-    describe('when 2 players seated', () => {
-      let hh;
-      beforeEach(() => {
-let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+      describe('when 2 players seated', () => {
+        let hh;
+        beforeEach(() => {
+  let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
 Table 'Aletheia' 6-max Seat #1 is the button
 Seat 1: Phil ($149.08 in chips) 
 Seat 6: reppinR1 ($246.97 in chips) 
@@ -123,16 +123,181 @@ Teiti14: posts small blind $0.50
 reppinR1: posts big blind $1
 *** HOLE CARDS ***
 Dealt to reppinR1 [3s 9c Ac Td]`
-        hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
-      })
+          hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+        })
 
-      it('should capture hero seat', () => {     
+        it('should capture hero seat', () => {     
+          expect(hh.hero.position).toEqual('BB');
+        });
+
+        it('should capture numbers of players seated', () => {     
+          expect(hh._players.length).toEqual(2);
+        });
+      });
+    });
+
+    describe('when second seat is the btn', () => {
+      // will check hero position and two other players for the sake of breviety
+      let hh;
+      beforeEach(() => {
+      let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+Table 'Aletheia' 6-max Seat #2 is the button
+Seat 1: Phil ($149.08 in chips) 
+Seat 2: marquim1980 ($117.77 in chips) 
+Seat 3: Yoo4 ($100 in chips) 
+Seat 4: Teiti14 ($27.49 in chips) 
+Seat 5: MiPwnYa ($107.49 in chips) 
+Seat 6: reppinR1 ($246.97 in chips) 
+Teiti14: posts small blind $0.50
+reppinR1: posts big blind $1
+*** HOLE CARDS ***
+  Dealt to reppinR1 [3s 9c Ac Td]`
+      hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+      });
+      
+      // hh.player array will always start with btn players
+      it('should find hero position', () => {     
+        expect(hh.hero.position).toEqual('MP');
+      });
+      
+      it('should find player on the button', () => {     
+        expect(hh.players[0].name).toEqual('marquim1980');
+      });
+      it('should find player on CO', () => {     
+        expect(hh.players[5].name).toEqual('Phil');
+      });
+
+    });
+
+    describe('when third seat is the btn', () => {
+      // will check hero position and two other players for the sake of breviety
+      let hh;
+      beforeEach(() => {
+      let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+Table 'Aletheia' 6-max Seat #3 is the button
+Seat 1: Phil ($149.08 in chips) 
+Seat 2: marquim1980 ($117.77 in chips) 
+Seat 3: Yoo4 ($100 in chips) 
+Seat 4: Teiti14 ($27.49 in chips) 
+Seat 5: MiPwnYa ($107.49 in chips) 
+Seat 6: reppinR1 ($246.97 in chips) 
+Teiti14: posts small blind $0.50
+reppinR1: posts big blind $1
+*** HOLE CARDS ***
+  Dealt to reppinR1 [3s 9c Ac Td]`
+      hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+      });
+      
+      // hh.player array will always start with btn players
+      it('should find hero position', () => {     
+        expect(hh.hero.position).toEqual('UTG');
+      });
+      
+      it('should find player on the button', () => {     
+        expect(hh.players[0].name).toEqual('Yoo4');
+      });
+      it('should find player on CO', () => {     
+        expect(hh.players[5].name).toEqual('marquim1980');
+      });
+
+    });
+
+    describe('when fourth seat is the btn', () => {
+      // will check hero position and two other players for the sake of breviety
+      let hh;
+      beforeEach(() => {
+      let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+Table 'Aletheia' 6-max Seat #4 is the button
+Seat 1: Phil ($149.08 in chips) 
+Seat 2: marquim1980 ($117.77 in chips) 
+Seat 3: Yoo4 ($100 in chips) 
+Seat 4: Teiti14 ($27.49 in chips) 
+Seat 5: MiPwnYa ($107.49 in chips) 
+Seat 6: reppinR1 ($246.97 in chips) 
+Teiti14: posts small blind $0.50
+reppinR1: posts big blind $1
+*** HOLE CARDS ***
+  Dealt to reppinR1 [3s 9c Ac Td]`
+      hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+      });
+      
+      // hh.player array will always start with btn players
+      it('should find hero position', () => {     
         expect(hh.hero.position).toEqual('BB');
       });
-
-      it('should capture numbers of players seated', () => {     
-        expect(hh._players.length).toEqual(2);
+      
+      it('should find player on the button', () => {     
+        expect(hh.players[0].name).toEqual('Teiti14');
       });
+      it('should find player on CO', () => {     
+        expect(hh.players[5].name).toEqual('Yoo4');
+      });
+    });
+
+    describe('when fith seat is the btn', () => {
+      // will check hero position and two other players for the sake of breviety
+      let hh;
+      beforeEach(() => {
+      let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+Table 'Aletheia' 6-max Seat #4 is the button
+Seat 1: Phil ($149.08 in chips) 
+Seat 2: marquim1980 ($117.77 in chips) 
+Seat 3: Yoo4 ($100 in chips) 
+Seat 4: Teiti14 ($27.49 in chips) 
+Seat 5: MiPwnYa ($107.49 in chips) 
+Seat 6: reppinR1 ($246.97 in chips) 
+Teiti14: posts small blind $0.50
+reppinR1: posts big blind $1
+*** HOLE CARDS ***
+  Dealt to reppinR1 [3s 9c Ac Td]`
+      hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+      });
+      
+      // hh.player array will always start with btn players
+      it('should find hero position', () => {     
+        expect(hh.hero.position).toEqual('SB');
+      });
+      
+      it('should find player on the button', () => {     
+        expect(hh.players[0].name).toEqual('MiPwnYa');
+      });
+      it('should find player on CO', () => {     
+        expect(hh.players[5].name).toEqual('Teiti14');
+      });
+
+    });
+
+    describe('when sixth seat is the btn', () => {
+      // will check hero position and two other players for the sake of breviety
+      let hh;
+      beforeEach(() => {
+      let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+Table 'Aletheia' 6-max Seat #4 is the button
+Seat 1: Phil ($149.08 in chips) 
+Seat 2: marquim1980 ($117.77 in chips) 
+Seat 3: Yoo4 ($100 in chips) 
+Seat 4: Teiti14 ($27.49 in chips) 
+Seat 5: MiPwnYa ($107.49 in chips) 
+Seat 6: reppinR1 ($246.97 in chips) 
+Teiti14: posts small blind $0.50
+reppinR1: posts big blind $1
+*** HOLE CARDS ***
+  Dealt to reppinR1 [3s 9c Ac Td]`
+      hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+      });
+      
+      // hh.player array will always start with btn players
+      it('should find hero position', () => {     
+        expect(hh.hero.position).toEqual('reppinR1');
+      });
+      
+      it('should find player on the button', () => {     
+        expect(hh.players[0].name).toEqual('reppinR1');
+      });
+      it('should find player on CO', () => {     
+        expect(hh.players[5].name).toEqual('MiPwnYa');
+      });
+
     });
   });
 });
