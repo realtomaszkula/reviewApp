dbeeb82ec0bff13dcccf0fdaee5ae663143a6c5e
Tomasz Kula <tomaszkula07@gmail.com>
2016-08-16 05:29:12 +0200

    Add new test

--- spec/setPlayers_spec.ts
+++ spec/setPlayers_spec.ts
@@ -300,5 +300,45 @@ reppinR1: posts big blind $1
       });
 
     });
+
+     describe('when setting players stack sizes', () => {
+       let hh;
+       beforeEach( () => {
+        let hhraw = `PokerStars Hand #156700709632:  Omaha Pot Limit ($0.50/$1.00 USD) - 2016/08/01 0:00:04 CET [2016/07/31 18:00:04 ET]
+Table 'Aletheia' 6-max Seat #1 is the button
+Seat 1: Phil ($149.08 in chips) 
+Seat 2: marquim1980 ($117.77 in chips) 
+Seat 3: Yoo4 ($100 in chips) 
+Seat 4: Teiti14 ($27.49 in chips) 
+Seat 5: MiPwnYa ($107.49 in chips) 
+Seat 6: reppinR1 ($246.97 in chips) 
+Teiti14: posts small blind $0.50
+reppinR1: posts big blind $1
+*** HOLE CARDS ***
+  Dealt to reppinR1 [3s 9c Ac Td]`
+        hh = new HandHistory({ hh: hhraw, options: { setHeroName: true, setPlayers: true, setHeroPosition: true } } );
+       });
+      it('should should set stacksize for first player', () => {     
+        expect(hh.players[0]).toEqual(149.08);
+      });
+      it('should should set stacksize for second player', () => {     
+        expect(hh.players[0]).toEqual(117.77);
+      });
+      it('should should set stacksize for third player', () => {     
+        expect(hh.players[0]).toEqual(149);
+      });
+      it('should should set stacksize for fourth player', () => {     
+        expect(hh.players[0]).toEqual(27.49);
+      });
+      it('should should set stacksize for fith player', () => {     
+        expect(hh.players[0]).toEqual(107.49);
+      });
+      it('should should set stacksize for sixth player', () => {     
+        expect(hh.players[0]).toEqual(246.97);
+      });
+      
+
+
+     });
   });
 });
